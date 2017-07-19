a198319a2ec036ea7960da144926fa7e479fe454
Pablo Henrick Diniz <phydokz@hotmail.com>
2017-07-19 08:13:22 -0400

    update

--- src/mouse.js
+++ src/mouse.js
@@ -1,33 +1,57 @@
-(function(window){
+(function(w){
     var Mouse = function (element) {
         var self = this;
-        initialize.apply(self);
+        initialize(self);
         self.element = element;
-        self.events = [];
+        self.listeners = [];
     };
 
-    Mouse.prototype.addEventListener = function(name,callback){
+    /**
+     *
+     * @param event
+     * @param params
+     */
+    Mouse.prototype.trigger = function(event,params){
         var self = this;
-        if(self.events[name] == undefined){
-            self.events[name] = [];
-        }
-        if(self.events[name].indexOf(callback) == -1){
-            self.events[name].push(callback);
-        }
-    };
-
-    Mouse.prototype.removeEventListener = function(name,callback){
-        var self = this;
-        if(self.events[name] != undefined){
-            var index = self.events[name].indexOf(callback);
-            if(index != -1){
-                self.events[name].splice(index,1);
+        if(self.listeners[event] != undefined){
+            var length = self.listeners[event].length;
+            for(var i =0; i < length;i++){
+                self.listeners[event][i].apply(self,params);
             }
         }
     };
 
-    var initialize = function(){
+    /**
+     *
+     * @param event
+     * @param callback
+     */
+    Mouse.prototype.addEventListener = function(event,callback){
         var self = this;
+        if(self.listeners[event] == undefined){
+            self.listeners[event] = [];
+        }
+        if(self.listeners[event].indexOf(callback) == -1){
+            self.listeners[event].push(callback);
+        }
+    };
+
+    /**
+     *
+     * @param event
+     * @param callback
+     */
+    Mouse.prototype.removeEventListener = function(event,callback){
+        var self = this;
+        if(self.listeners[event] != undefined){
+            var index = self.listeners[event].indexOf(callback);
+            if(index != -1){
+                self.listeners[event].splice(index,1);
+            }
+        }
+    };
+
+    function initialize(self){
         var element = null;
         var left = false;
         var middle = false;
@@ -41,103 +65,106 @@
         var lastY = 0;
 
 
-        var mousemove = function(event){
+        function mousemove(event){
+            if(event.target == element){
+                event.preventDefault();
+                var x = event.offsetX;
+                var y = event.offsetY;
+                lastX = x;
+                lastY = y;
+                self.trigger('mousemove',[x,y,event]);
+            }
+        }
+
+        function mousedown(event){
+            if(event.target == element){
+                event.preventDefault();
+                lastDownX = event.offsetX;
+                lastDownY = event.offsetY;
+
+                switch(event.which){
+                    case 1:
+                        left = true;
+                        break;
+                    case 2:
+                        middle = true;
+                        break;
+                    case 3:
+                        right = true;
+                        break;
+                }
+
+                self.trigger('mousedown',[lastDownX,lastDownY,event]);
+                return false;
+            }
+        }
+
+        function mouseenter(event){
             event.preventDefault();
-            var x = event.offsetX;
-            var y = event.offsetY;
-            lastX = x;
-            lastY = y;
-            if(self.events['mousemove'] != undefined){
-                call_events(self.events['mousemove'],self,[x,y,event]);
+            if(self.listeners['mouseenter'] != undefined){
+                call_events(self.listeners['mouseenter'],self, [event]);
             }
-        };
+        }
 
-        var mousedown=function(event){
-            event.preventDefault();
-            lastDownX = event.offsetX;
-            lastDownY = event.offsetY;
-
-            switch(event.which){
-                case 1:
-                    left = true;
-                    break;
-                case 2:
-                    middle = true;
-                    break;
-                case 3:
-                    right = true;
-                    break;
-            }
-
-            if(self.events['mousedown'] != undefined){
-                call_events(self.events['mousedown'],self,[lastDownX,lastDownY,event]);
-            }
-        };
-
-        var mouseout =function(event){
-            event.preventDefault();
-            left = false;
-            right = false;
-            middle = false;
-            if(self.events['mouseout'] != undefined){
-                call_events(self.events['mouseout'],self, [event]);
-            }
-        };
-
-        var mouseenter = function(event){
-            event.preventDefault();
-            if(self.events['mouseenter'] != undefined){
-                call_events(self.events['mouseenter'],self, [event]);
-            }
-        };
-
-        var mousewheel = function(e){
+        function mousewheel(e){
             e.preventDefault();
             var wheel = e.detail ? e.detail * (-120) : e.wheelDelta;
             if(/Firefox/i.test(navigator.userAgent)){
                 wheel = (wheel / 360)*120;
             }
             lastWheel = wheel;
-            if(self.events['mousewheel'] != undefined){
-                call_events(self.events['mousewheel'],self, [wheel,e]);
+            if(self.listeners['mousewheel'] != undefined){
+                call_events(self.listeners['mousewheel'],self, [wheel,e]);
             }
-        };
+        }
 
-        var mouseup = function (event) {
-            event.preventDefault();
-            event.stopPropagation();
-            if (element !== null) {
-                lastUpX = event.offsetX;
-                lastUpY = event.offsetY;
-
-                switch (event.which) {
-                    case 1:
-                        left = false;
-                        break;
-                    case 2:
-                        middle = false;
-                        break;
-                    case 3:
-                        right = false;
-                }
-
-                if(self.events['mouseup'] != undefined){
-                    call_events(self.events['mouseup'],self, [lastUpX,lastUpY,event]);
-                }
+        function mouseout(event){
+            if(event.target == element){
+                event.preventDefault();
+                left = false;
+                right = false;
+                middle = false;
+                self.trigger('mouseout',[event]);
+                return false;
             }
-        };
+        }
 
-        var contextmenu =  function(e){
+        function mouseup(event) {
+            if(event.target == element){
+                event.preventDefault();
+                if (element !== null) {
+                    lastUpX = event.offsetX;
+                    lastUpY = event.offsetY;
+
+                    switch (event.which) {
+                        case 1:
+                            left = false;
+                            break;
+                        case 2:
+                            middle = false;
+                            break;
+                        case 3:
+                            right = false;
+                    }
+
+                    self.trigger('mouseup',[lastUpX,lastUpY,event]);
+                }
+                return false;
+            }
+        }
+
+        function contextmenu(e){
             e.preventDefault();
-        };
+            return false;
+        }
 
-        var windowblur = function(e){
+        function windowblur(e){
             left = false;
             right = false;
             middle = false;
-        };
+        }
 
-        window.addEventListener('blur',windowblur);
+        w.addEventListener('blur',windowblur);
 
         var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel";
 
@@ -233,15 +260,8 @@
                 return lastY;
             }
         });
-    };
+    }
 
-    var call_events = function(events,context,params){
-        var length = events.length;
-        var i;
-        for(i =0; i < length;i++){
-            events[i].apply(context,params);
-        }
-    };
 
-    window.Mouse = Mouse;
+    w.Mouse = Mouse;
 })(window);
